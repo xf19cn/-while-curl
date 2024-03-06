a716fcc9983eebd484d02eb43a3ffff9677018a9
xf19cn <158791235+xf19cn@users.noreply.github.com>
2024-03-06 13:13:06 +0800

    Update curlrequest.sh

--- curlrequest.sh
+++ curlrequest.sh
@@ -1,6 +1,7 @@
 while true; do
 wget https://mjt.jd.com/consumer/jintiao/goldbullion/index.html target -O dd
 wget m.jr.jd.com target -O dd
+rm dd
   if [ "$condition" = "true" ]; then
     exit 0
   fi
