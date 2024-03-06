23681f78c544da69d5d565d7b19dbb988d0628d1
xf19cn <158791235+xf19cn@users.noreply.github.com>
2024-03-06 17:11:52 +0800

    Update request.sh

--- request.sh
+++ request.sh
@@ -1,6 +1,5 @@
 while true; do
-wget https://mjt.jd.com/consumer/jintiao/goldbullion/index.html target -O dd
-rm dd
+wget https://mjt.jd.com/consumer/jintiao/goldbullion/index.html -O dd
   if [ "$condition" = "true" ]; then
     exit 0
   fi
