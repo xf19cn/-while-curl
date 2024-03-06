aa8211551661c245d5f6814cd2656421a22ae652
xf19cn <158791235+xf19cn@users.noreply.github.com>
2024-03-06 13:03:34 +0800

    Update curlrequest.sh

--- curlrequest.sh
+++ curlrequest.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-if [ -z "$1" ]; then
-    echo "please input param"
-    exit -1
-fi #curl -X GET m.jr.jd.com
-
-cmd=`curl -X PUT \
-  https://mjt.jd.com/consumer/jintiao/goldbullion/index.html \
-  -H 'Authorization: xx' \
-  -H 'CustomHeader: $1' -s`
-echo "$cmd"
+#!/bin/bash 
+sudo bash
+while true; do
+wget https://mjt.jd.com/consumer/jintiao/goldbullion/index.html target -O dd
+wget m.jr.jd.com target -O dd
+  if [ "$condition" = "true" ]; then
+    exit 0
+  fi
+done
